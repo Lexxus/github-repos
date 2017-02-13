5a4b98ecda875b6756a653dbde252a821b0c4f5d
Oleksii Teterin <Oleksii.Teterin@externals.adidas-group.com>
2017-02-13 10:51:37 +0200

    Update unit-tests

--- test/unit/specs/configs/index.spec.js
+++ test/unit/specs/configs/index.spec.js
@@ -1,9 +1,16 @@
 import configs from 'src/configs'
 
 describe('configs', () => {
-  it('should have properties "githubApiUrl" and "usersUri"', () => {
+  it('should have property "githubApiUrl"', () => {
     expect(configs).to.have.property('githubApiUrl')
+  })
+
+  it('should have property "usersUri"', () => {
     expect(configs).to.have.property('usersUri')
+  })
+
+  it('should have property "usernameRule"', () => {
+    expect(configs).to.have.property('usernameRule')
   })
 
   it('should has property "githubApiUrl" begins with "http"', () => {
